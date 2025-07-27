e76eede3540e71b7363822883219f747ce637ae4
lelikone777 <2257855@gmail.com>
2025-07-28 01:33:22 +0300

    vercel bug fix 2

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -16,7 +16,7 @@ export async function handleSubmission(formData: FormData) {
   const content = formData.get('content');
   const imageUrl = formData.get('imageUrl');
 
-  const data = await prisma.blogPost.create({
+  await prisma.blogPost.create({
     data: {
       title: title as string,
       content: content as string,
